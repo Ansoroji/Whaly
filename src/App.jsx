82eee6fec08ba5313e9056f86c83f4c9e57c7517
Ansoroji <anasofiarobledo@gmail.com>
2026-09-08 13:47:55 -0500

    Ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import {
   Routes,
   Route,
-  Link,
 } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
-import SeccionServicios from "./components/SeccionServicios/SeccionServicios";
-import SeccionContacto from "./components/SeccionContacto/SeccionContacto";
-import Footer from "./components/Footer/Footer";
 
+import PaginaInicio from "./pages/PaginaInicio/PaginaInicio";
 import SolucionesPersonas from "./pages/SolucionesPersonas/SolucionesPersonas";
 import SolucionesEmpresas from "./pages/SolucionesEmpresas/SolucionesEmpresas";
 
@@ -18,68 +15,6 @@ import Empresas from "./pages/Empresas/Empresas";
 import "./App.css";
 
 
-function PaginaInicio() {
-  return (
-    <div className="landing-container">
-
-      {/*HERO*/}
-      <main
-        className="hero"
-        id="inicio"
-      >
-        <span className="greeting">
-          HELLOOO ¿CÓMO VAS?
-        </span>
-
-        <h1 className="title">
-          Bienvenido a
-          <br />
-
-          <span className="brand-highlight">
-            Whaly
-          </span>
-        </h1>
-
-        <p className="subtitle">
-          La agencia de seguros más cool de Colombia
-        </p>
-
-        <p className="description">
-          Tenemos más de 30 años de experiencia ayudando a las personas
-        </p>
-
-
-        {/*BOTONES*/}
-        <div className="button-group">
-
-          <Link
-            to="/soluciones-personas"
-            className="btn-personas"
-          >
-            Soluciones para personas
-          </Link>
-
-          <Link
-            to="/soluciones-empresas"
-            className="btn-empresas"
-          >
-            Soluciones para empresas
-          </Link>
-
-        </div>
-
-      </main>
-
-
-      <SeccionServicios />
-
-      <SeccionContacto />
-
-      <Footer />
-
-    </div>
-  );
-}
 
 
 function App() {
